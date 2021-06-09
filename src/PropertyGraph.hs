a540e2d64c3f433024094ae14a209a6a4dadff57
Arnau Abella <arnauabella@gmail.com>
2021-06-09 22:26:19 +0200

    more work on property graph

--- src/PropertyGraph.hs
+++ src/PropertyGraph.hs
@@ -6,6 +6,15 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE DisambiguateRecordFields #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE LambdaCase #-}
 {-# OPTIONS_GHC -Wincomplete-patterns#-}
 module PropertyGraph where
 
@@ -14,6 +23,8 @@ module PropertyGraph where
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Generic hiding (StreamF(..))
+import Data.String
+import qualified Data.List as List
 
 --------------------------------------------
 
@@ -40,18 +51,133 @@ Graph Metrics:
 data Value
   = VString String
   | VInt Int
+  deriving stock (Eq)
+instance Show Value where
+  show  = \case
+    VString s -> show s
+    VInt s -> show s
 
-data PropertyGraphF r =
-    Node { node :: (String, Map String Value)
-         , edges :: [r]
-         }
+instance IsString Value where
+  fromString = VString
+
+type Properties = Map String Value
+
+showProperties :: Properties -> String
+showProperties props
+  | null props = ""
+  | otherwise =
+      let xs = take 2 $ Map.toList props
+      in "{" ++ showPairs xs ++ dots props ++ "}"
+  where
+    showPair (k, v) = show k ++ ":" ++ show v
+    showPairs = List.intercalate "," . fmap showPair
+    dots xs = if length xs > 2 then "..." else ""
+
+newtype Label = Label { unLabel :: String}
+  deriving stock (Show)
+  deriving newtype (Eq, Ord, IsString)
+
+data Edge r = Edge
+  { label :: Label
+  , properties :: Properties
+  , node :: r
+  }
+  deriving stock (Show)
+  deriving stock (Functor, Foldable, Traversable)
+
+showEdge :: (r -> String) -> Edge r -> String
+showEdge sh Edge{label, properties, node} =
+  "[" ++ unLabel label ++ showProperties properties ++ "]" ++ "->" ++ sh node
+
+data PropertyGraphF r = Node
+  { label :: Label,
+    properties :: Properties,
+    edges :: [Edge r]
+  }
   deriving (Functor, Foldable, Traversable)
 
 type PropertyGraph = Graph PropertyGraphF
 
--- (author:Author {name: "Arnau"})
-pg1 = Hide (In (Node {node = ("Author", Map.empty), edges = []}))
+instance ShowF PropertyGraphF where
+  showF sh Node {label, properties, edges} =
+    "("
+      ++ unLabel label
+      ++ showProperties properties
+      ++ ")"
+      ++ if null edges then "" else "-"
+      ++ List.intercalate "\n-" (fmap (showEdge sh) edges)
 
--- (a: Author {name: "Arnau"}) - [r: AuthorOf] - (b: Book {title: "Final Project"})
-pg2 = Hide (In (Node {node = ("Author", Map.empty),
-                      edges = []}))
+pg1 =
+  Hide
+    ( In
+        ( Node
+            { label = "Author",
+              properties = [("name", "Arnau")],
+              edges = []
+            }
+        )
+    )
+
+-- >>> putStrLn $ showGraph pg2
+pg2 =
+  Hide
+    ( In
+        ( Node
+            { label = "Author",
+              properties = [("name", "Nietzsche")],
+              edges =
+                [ Edge
+                    { label = "AuthorOf",
+                      properties = [],
+                      node =
+                        In
+                          ( Node
+                              { label = "Book",
+                                properties = [("title", "Also sprach Zarathustra")],
+                                edges = []
+                              }
+                          )
+                    }
+                ]
+            }
+        )
+    )
+
+-- Mutually recursive edges
+-- Multi-edge
+
+pg3 =
+  Hide
+    ( Mu
+      ( \(~(john : nancy : _)) ->
+          [
+            Node {
+              label = "Person",
+              properties = [("name", "John")],
+              edges =
+                [ Edge
+                    { label = "fatherOf",
+                      properties = [],
+                      node = Var nancy
+                    },
+                  Edge
+                    { label = "livingWith",
+                      properties = [],
+                      node = Var nancy
+                    }
+                ]
+            },
+            Node {
+              label = "Person",
+              properties = [("name", "Nancy")],
+              edges =
+                [ Edge
+                    { label = "daughterOf",
+                      properties = [],
+                      node = Var john
+                    }
+                ]
+            }
+          ]
+      )
+    )
